527c9653b26d71670bf8fc95986c3f5da7d8e687
Timofey Makhlay <tmakhlay2@gmail.com>
2019-01-29 15:06:44 -0800

    Added table view (programmatically)

---
+++ ProductHunt-API-Network-Layer/ProductHunt-API-Network-Layer/TableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  TableViewCell.swift
+//  ProductHunt-API-Network-Layer
+//
+//  Created by timofey makhlay on 1/29/19.
+//  Copyright © 2019 Timofey Makhlay. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    var name: UITextView = {
+        var title = UITextView()
+        title.text = "name"
+        title.font = UIFont(name: "AvenirNext-Bold", size: 25)
+        title.textColor = #colorLiteral(red: 0.1075617597, green: 0.09771008044, blue: 0.1697227657, alpha: 1)
+        title.backgroundColor = nil
+        title.textAlignment = .center
+        title.isEditable = false
+        title.isScrollEnabled = false
+        title.isSelectable = false
+        return title
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(name)
+        name.centerVerticalOfView(to: self)
+        name.anchor(top: nil, leading: leadingAnchor, bottom: nil, trailing: nil,padding: .init(top: 0, left: 15, bottom: 0, right: 0))
+    }
+    
+    // Required with initilizer
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
